661737e743a2e5c17795a74528ea8b4740fce7ac
asarepaulboharm <2425400671@live.gctu.edu.gh>
2026-06-03 12:48:47 +0000

    Assignment1

--- initials.py
+++ initials.py
@@ -1,7 +1,30 @@
+# File: initials.py
+# Description: Prints stylized large initials P.A.A. with strict dimension and syntax constraints.
+# Assignment Number: 1
+#
+# Name: Paul Asare
+# STUDENT ID: 2425400671
+# Email: 2425400671@live.gctu.edu.gh
+# Grader: Mr. Augustus Buckman
+#
+# On my honor, Paul Asare, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
-
-
+    """Prints small and large stylized initials using limited string literals."""
+    print()
+    print("...PAA")
+    print()
+    print("...PPPPPPPPPPPP............AAAAAAAA..............AAAAAAAA........")
+    print("...PPPPPPPPPPPPP..........AAAAAAAAAA............AAAAAAAAAA.......")
+    print("...PP........PPP.........AAA......AAA..........AAA......AAA......")
+    print("...PP........PPP........AAA........AAA........AAA........AAA.....")
+    print("...PPPPPPPPPPPPP........AAA........AAA........AAA........AAA.....")
+    print("...PPPPPPPPPPPP.........AAAAAAAAAAAAAA........AAAAAAAAAAAAAA.....")
+    print("...PP...................AAAAAAAAAAAAAA........AAAAAAAAAAAAAA.....")
+    print("...PP...................AAA........AAA........AAA........AAA.....")
+    print("...PP..............**...AAA........AAA...**...AAA........AAA...**")
+    print("...PP..............**...AAA........AAA...**...AAA........AAA...**")
+    print()
 main()
